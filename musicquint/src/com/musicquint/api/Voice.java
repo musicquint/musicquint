507f3cbf39c733c5f08c41530603a4610c0b5766
Andreas <andreas.goss1994@gmail.com>
2021-07-05 22:17:36 +0200

    Javadoc for Voice

--- musicquint/src/com/musicquint/api/Voice.java
+++ musicquint/src/com/musicquint/api/Voice.java
@@ -1,7 +1,10 @@
 package com.musicquint.api;
 
+import static com.musicquint.api.BarTime.add;
+import static com.musicquint.api.BarTime.subtract;
+import static com.musicquint.api.Measurable.canonicalComparator;
+
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.NavigableMap;
 import java.util.Objects;
 import java.util.Optional;
@@ -18,20 +21,19 @@ import java.util.stream.Stream;
  * ContentSets} and offers a variety of default methods for convenience. Another
  * way to view a voice is to see it as NavigableMap which maps a BarTime to
  * multiple {@linkplain ContentItem ContentItems} to support this view the
- * interface offers additional methods {@link #put(BarTime, PrincipalItem)},
- * {@link #put(BarTime, PrincipalItem...)} and
+ * interface offers additional methods {@link #put(BarTime, PrincipalItem)}, and
  * {@link #put(BarTime, OptionalItem)}.
  * </p>
  * As a {@link ContentSet} is also a {@linkplain Measurable} object special
  * constraints are put upon all ContentItem or ContentSet with a duration
  * {@code d} that are to be added into a Voice at any given BarTime {@code t}
- * (Note that we use the {@code +} and {@code -} operator as abbreviation for
+ * (Note that we use the {@code +} and {@code -} operator as abbreviations for
  * the {@link BarTime#add(BarTime, BarTime)} and
  * {@link BarTime#subtract(BarTime, BarTime)} methods):
  * <ul>
  * <li>After adding the item to the voice no other item can be added to the
- * voice from the time {@code t} up to {@code t + d}. Interval boundaries are
- * excluded from this rule. A default implementation of
+ * voice for all BarTimes {@code t} in the interval {@code (t, t + d)}. Interval
+ * boundaries are excluded from this rule. A default implementation of
  * {@link #lasting(BarTime)} is provided to check this condition.</li>
  * <li>If the voice has a higher key {@code t'} that is higher than t. Then in
  * accordance with the first constraint the BarTime {@code t + d} needs to be
@@ -40,24 +42,92 @@ import java.util.stream.Stream;
  * this condition.</li>
  * </ul>
  * A default implementation {@link #fits(BarTime, Measurable)} is provided to
- * check both conditions. Also we define a Voice to be consistent if
- * {@code lasting(t) == next(t)} for every {@code t} in the interval
- * [0, {@link #capacity()}].
+ * check both conditions. Also we define a Voice to be consistent stuffed or
+ * well-stuffed, if {@code lasting(t) == next(t)} for every {@code t} in the
+ * interval [0, {@link #capacity()}].
  */
 public interface Voice extends NavigableMap<BarTime, Voice.ContentSet<PrincipalItem>> {
 
+    /**
+     * Associates the key with the given ContentSet in the voice. To ensure
+     * consistency of data in the voice the given implementation must ensure that
+     * special constraint are always honored while adding data to the voice. Those
+     * can be checked with the default implementation of
+     * {@link #fits(BarTime, Measurable)}.
+     *
+     * @param key   the BarTime with which the specified ContentSet is to be
+     *              associated
+     * @param value the given ContentSet that is to be mapped to the given key
+     * @return the previous ContentSet associated with {@code key}, or {@code null}
+     *         if there was no mapping for {@code key}.
+     * @throws NullPointerException  if the given key or value is null.
+     * @throws IllegalStateException if {@link #fits(BarTime, Measurable)} is false
+     *                               and for the key and value and at least one of
+     *                               the constraints that a voice must obey is
+     *                               violated.
+     * @see #fits(BarTime, Measurable)
+     */
+    @Override
     Voice.ContentSet<PrincipalItem> put(BarTime key, Voice.ContentSet<PrincipalItem> value);
 
+    /**
+     * Associates the key with a ContentSet that contains the given PrincipalItem.
+     * If there is also a ContentSet associated with the given key then the
+     * PrincipalItem is added to the ContentSet. If no ContentSet was previously
+     * associated to the key a new ContentSet is created and added. To ensure
+     * consistency of data in the voice the given implementation must ensure that
+     * special constraint are always honored while adding data to the voice. Those
+     * can be checked with the default implementation of
+     * {@link #fits(BarTime, Measurable)}.
+     *
+     * @param key  the BarTime with which the specified ContentSet is to be
+     *             associated
+     * @param item the given PrincipalItem that is to be added.
+     * @throws NullPointerException     if the given key or item is null.
+     * @throws IllegalStateException    if {@link #fits(BarTime, Measurable)} is
+     *                                  false and for the key or item at least one
+     *                                  of the constraints, that a voice must obey,
+     *                                  is violated.
+     * @throws IllegalArgumentException if the given key is less than zero or
+     *                                  greater than the capacity of the voice.
+     * @see #fits(BarTime, Measurable)
+     */
     void put(BarTime key, PrincipalItem item);
 
+    /**
+     * Associates the key with ContentSet that contains the given OptionalItem. If
+     * there is also a ContentSet associated with the given key then the
+     * OptionalItem is added to the ContentSet. If no ContentSet was previously
+     * associated to the key a new ContentSet is created and added. Note that a
+     * OptionalItem does not add to the duration of the ContentSet and therefore the
+     * only condition that must be satisfied is {@code lasting(key) == BarTime.ZERO}
+     *
+     * @param key  the BarTime with which the specified ContentSet is to be
+     *             associated
+     * @param item the given OptionalItem that is to be added.
+     * @throws NullPointerException     if the given key or item is null.
+     * @throws IllegalStateException    if {@code lasting(key) != BarTime.ZERO}
+     * @throws IllegalArgumentException if the given key is less than zero or
+     *                                  greater than the capacity of the voice.
+     * @see #lasting(BarTime)
+     * @see #capacity()
+     */
     void put(BarTime key, OptionalItem item);
 
+    /**
+     * The capacity is an upper bound for all key that are to be inserted into the
+     * voice. It is advised that the capacity is immutable to avoid the corruption
+     * of data although it is not inconceivable that one might implement the
+     * capacity in such a way, that all voice entries are removed if they exceed the
+     * capacity of the voice after alteration.
+     *
+     * @return the capacity of the voice as BarTime
+     */
     BarTime capacity();
 
     default void put(BarTime key, Collection<PrincipalItem> items) {
         Objects.requireNonNull(items);
-        Comparator<Measurable> comparator = Measurable.canonicalComparator();
-        items.stream().sorted(comparator.reversed()).forEach(i -> put(key, i));
+        items.stream().sorted(canonicalComparator().reversed()).forEach(i -> put(key, i));
     }
 
     default void put(BarTime key, PrincipalItem... items) {
@@ -76,19 +146,15 @@ public interface Voice extends NavigableMap<BarTime, Voice.ContentSet<PrincipalI
         if (lowerEntry == null) {
             return BarTime.ZERO;
         } else {
-            BarTime endLower = BarTime.add(lowerEntry.getKey(), lowerEntry.getValue());
-            return BarTime.max(BarTime.ZERO, BarTime.subtract(endLower, key));
+            BarTime endLower = add(lowerEntry.getKey(), lowerEntry.getValue());
+            return BarTime.max(BarTime.ZERO, subtract(endLower, key));
         }
     }
 
     default BarTime next(BarTime key) {
         Objects.requireNonNull(key);
         BarTime higherKey = higherKey(key);
-        if (higherKey != null) {
-            return BarTime.subtract(higherKey, key);
-        } else {
-            return BarTime.subtract(capacity(), key);
-        }
+        return higherKey != null ? subtract(higherKey, key) : subtract(capacity(), key);
     }
 
     default BarTime length() {
@@ -96,7 +162,7 @@ public interface Voice extends NavigableMap<BarTime, Voice.ContentSet<PrincipalI
             return BarTime.ZERO;
         } else {
             Entry<BarTime, ContentSet<PrincipalItem>> lastEntry = lastEntry();
-            return BarTime.add(lastEntry.getKey(), lastEntry.getValue());
+            return add(lastEntry.getKey(), lastEntry.getValue());
         }
     }
 
@@ -116,7 +182,7 @@ public interface Voice extends NavigableMap<BarTime, Voice.ContentSet<PrincipalI
         }
 
         default boolean isRest() {
-            return !isPitched();
+            return !isPitched() && !isEmpty();
         }
 
         default boolean isChord() {
