a698e48ac4ec2272b5a1fe7ba6bf758c9ac9332d
Andreas <andreas.goss1994@gmail.com>
2021-07-06 20:05:54 +0200

    Javadoc for additional methods in Voice

--- musicquint/src/com/musicquint/api/Voice.java
+++ musicquint/src/com/musicquint/api/Voice.java
@@ -60,11 +60,13 @@ public interface Voice extends NavigableMap<BarTime, Voice.ContentSet<PrincipalI
      * @param value the given ContentSet that is to be mapped to the given key
      * @return the previous ContentSet associated with {@code key}, or {@code null}
      *         if there was no mapping for {@code key}.
-     * @throws NullPointerException  if the given key or value is null.
-     * @throws IllegalStateException if {@link #fits(BarTime, Measurable)} is false
-     *                               and for the key and value and at least one of
-     *                               the constraints that a voice must obey is
-     *                               violated.
+     * @throws NullPointerException          if the given key or value is null.
+     * @throws IllegalStateException         if {@link #fits(BarTime, Measurable)}
+     *                                       is false and for the key and value and
+     *                                       at least one of the constraints that a
+     *                                       voice must obey is violated.
+     * @throws UnsupportedOperationException if the voice does not support the
+     *                                       Operation.
      * @see #fits(BarTime, Measurable)
      */
     @Override
@@ -80,16 +82,18 @@ public interface Voice extends NavigableMap<BarTime, Voice.ContentSet<PrincipalI
      * can be checked with the default implementation of
      * {@link #fits(BarTime, Measurable)}.
      *
-     * @param key  the BarTime with which the specified ContentSet is to be
+     * @param key  the BarTime with which the specified PrincipalItem is to be
      *             associated
      * @param item the given PrincipalItem that is to be added.
-     * @throws NullPointerException     if the given key or item is null.
-     * @throws IllegalStateException    if {@link #fits(BarTime, Measurable)} is
-     *                                  false and for the key or item at least one
-     *                                  of the constraints, that a voice must obey,
-     *                                  is violated.
-     * @throws IllegalArgumentException if the given key is less than zero or
-     *                                  greater than the capacity of the voice.
+     * @throws NullPointerException          if the given key or item is null.
+     * @throws IllegalStateException         if {@link #fits(BarTime, Measurable)}
+     *                                       is false and for the key or item at
+     *                                       least one of the constraints, that a
+     *                                       voice must obey, is violated.
+     * @throws IllegalArgumentException      if the given key is less than zero or
+     *                                       greater than the capacity of the voice.
+     * @throws UnsupportedOperationException if the voice does not support the
+     *                                       Operation.
      * @see #fits(BarTime, Measurable)
      */
     void put(BarTime key, PrincipalItem item);
@@ -100,15 +104,18 @@ public interface Voice extends NavigableMap<BarTime, Voice.ContentSet<PrincipalI
      * OptionalItem is added to the ContentSet. If no ContentSet was previously
      * associated to the key a new ContentSet is created and added. Note that a
      * OptionalItem does not add to the duration of the ContentSet and therefore the
-     * only condition that must be satisfied is {@code lasting(key) == BarTime.ZERO}
+     * only condition that must be satisfied is
+     * {@code lasting(key) == BarTime.ZERO}.
      *
-     * @param key  the BarTime with which the specified ContentSet is to be
+     * @param key  the BarTime with which the specified OptionalItem is to be
      *             associated
      * @param item the given OptionalItem that is to be added.
-     * @throws NullPointerException     if the given key or item is null.
-     * @throws IllegalStateException    if {@code lasting(key) != BarTime.ZERO}
-     * @throws IllegalArgumentException if the given key is less than zero or
-     *                                  greater than the capacity of the voice.
+     * @throws NullPointerException          if the given key or item is null.
+     * @throws IllegalStateException         if {@code lasting(key) != BarTime.ZERO}
+     * @throws IllegalArgumentException      if the given key is less than zero or
+     *                                       greater than the capacity of the voice.
+     * @throws UnsupportedOperationException if the voice does not support the
+     *                                       Operation.
      * @see #lasting(BarTime)
      * @see #capacity()
      */
@@ -121,15 +128,58 @@ public interface Voice extends NavigableMap<BarTime, Voice.ContentSet<PrincipalI
      * capacity in such a way, that all voice entries are removed if they exceed the
      * capacity of the voice after alteration.
      *
-     * @return the capacity of the voice as BarTime
+     * @return the capacity of the voice as BarTime.
      */
     BarTime capacity();
 
+    /**
+     * Default implementation for adding a Collection of PrincipalItem to the voice
+     * at the specified BarTime key. The items are ordered in reverse and the
+     * biggest measurable is added first to the voice. The default implementation
+     * relies on the implementation of {@link #put(BarTime, PrincipalItem)}.
+     *
+     * @param key   the BarTime with which the specified PrincipalItem is to be
+     *              associated
+     * @param items the given collection of PrincipalItems that are to be added.
+     * @throws NullPointerException          if the key or collection of items is
+     *                                       null or one of the items in the
+     *                                       collection is null.
+     * @throws IllegalStateException         if {@link #fits(BarTime, Measurable)}
+     *                                       is false and for the key or at least
+     *                                       one item in the collection.
+     * @throws IllegalArgumentException      if the given key is less than zero or
+     *                                       greater than the capacity of the voice.
+     * @throws UnsupportedOperationException if the voice does not support the
+     *                                       Operation
+     *                                       {@link #put(BarTime, PrincipalItem)}
+     * @see #put(BarTime, PrincipalItem)
+     */
     default void put(BarTime key, Collection<PrincipalItem> items) {
         Objects.requireNonNull(items);
         items.stream().sorted(canonicalComparator().reversed()).forEach(i -> put(key, i));
     }
 
+    /**
+     * Default implementation for adding an array of PrincipalItem to the voice at
+     * the specified BarTime key. The items are ordered in reverse and the biggest
+     * measurable is added first to the voice. The default implementation relies on
+     * the implementation of {@link #put(BarTime, Collection)}.
+     *
+     * @param key   the BarTime with which the specified PrincipalItem is to be
+     *              associated
+     * @param items the given array of PrincipalItems that are to be added.
+     * @throws NullPointerException          if the key or array of items is null or
+     *                                       one of the items in the array is null
+     * @throws IllegalStateException         if {@link #fits(BarTime, Measurable)}
+     *                                       is false and for the key or at least
+     *                                       one item in the collection.
+     * @throws IllegalArgumentException      if the given key is less than zero or
+     *                                       greater than the capacity of the voice.
+     * @throws UnsupportedOperationException if the voice does not support the
+     *                                       Operation
+     *                                       {@link #put(BarTime, PrincipalItem)}
+     * @see #put(BarTime, PrincipalItem)
+     */
     default void put(BarTime key, PrincipalItem... items) {
         put(key, Stream.of(items).collect(Collectors.toSet()));
     }
@@ -166,6 +216,11 @@ public interface Voice extends NavigableMap<BarTime, Voice.ContentSet<PrincipalI
         }
     }
 
+    /**
+     *
+     *
+     * @param <T>
+     */
     interface ContentSet<T extends ContentItem> extends Set<T>, Measurable {
 
         default SortedSet<Pitch> getPitches() {
